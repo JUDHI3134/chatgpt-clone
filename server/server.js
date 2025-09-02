91f7a81b96b64909aaa4ed77cc8b263d5a262e22
JUDHI3134 <judhistirbehera456@gmail.com>
2025-09-02 21:00:46 +0530

    create stripe webhooks success event

--- server/server.js
+++ server/server.js
@@ -6,10 +6,14 @@ import userRouter from "./routes/userRoutes.js";
 import chatRouter from "./routes/chatRoutes.js";
 import messageRouter from "./routes/messageRoutes.js";
 import creditRouter from "./routes/creditRoutes.js";
+import { stripeWebhooks } from "./controllers/webhooks.js";
 
 const app = express();
 
 await connectDB();
+
+//stripe webhook
+app.post("/api/stripe", express.raw({type: 'application/json'}), stripeWebhooks)
 
 //middlewares
 app.use(cors());
